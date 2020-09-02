156444bf691863876bf629d53b4e0179a4b800fb
Lukas Koucky <lukoucky@gmail.com>
2020-09-02 22:45:17 +0200

    Flatten feature vector

--- backend/imagenet_similarity.py
+++ backend/imagenet_similarity.py
@@ -1,20 +1,28 @@
 from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+from keras.applications.nasnet import NASNetLarge
+from keras.applications.nasnet import preprocess_input, decode_predictions
+
+from keras.preprocessing import image
 from keras.models import Model
 import numpy as np
+
 from os.path import join
 from utils import is_image, get_all_images_in_dir
 
 resnet50_model = ResNet50(weights='imagenet')
 
 def predict(img_path : str, model: Model):
+    print('Predicting',img_path)
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    print('Predicting image', img_path)
-    return model.predict(x)
+    features = model.predict(x)
+    flattened_features = features.flatten()
+    normalized_features = flattened_features / np.linalg.norm(flattened_features)
+    return normalized_features
 
 def predict_resnet50(img_path: str):
     return predict(img_path, resnet50_model)
@@ -30,19 +38,22 @@ def findDifferences(feature_vectors, image_name):
             similar[k] = diff
     return similar 
 
-def compute_similarity(image_name):
+def similarity_resnet50(image_name):
+    model = ResNet50(weights='imagenet')
+    return compute_similarity(image_name, model)
+
+def similarity_nasnet_large(image_name):
+    model = NASNetLarge(weights='imagenet')
+    return compute_similarity(image_name, model)
+
+def compute_similarity(image_name, model):
     img_dir = 'images'
     img_id = 0
     print('compute_similarity for image', image_name)
     feature_vectors: dict = {}
-    model = ResNet50(weights='imagenet')
     for i, img_path in enumerate(get_all_images_in_dir(img_dir, True)):
             feature_vectors[img_path] = predict(img_path,model)[0]
 
     results=findDifferences(feature_vectors, image_name)
     print(results)
     return results
-    # for k,v in results.items():
-    #     print(k +" is most similar to: "+ v)    
-    # #print('Predicted:', decode_predictions(preds, top=3)[0])
-
